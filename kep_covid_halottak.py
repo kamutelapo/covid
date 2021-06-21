3f5d327343e071d60dfb0a0ac2a1629948f6a2e1
Kamu Telapo <kamutelapo@freemail.hu>
2021-06-21 20:00:46 +0200

    Elhunytak átlaga

---
+++ kep_covid_halottak.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+import numpy as np
+import datetime as dt
+import os
+
+BASEDIR=os.path.dirname(__file__)
+
+df = pd.read_csv(BASEDIR +"/covidadatok.csv", parse_dates=['Dátum'])
+df['Hét kezdet'] = df.apply(lambda row: row['Dátum'] - dt.timedelta(days=row['Dátum'].weekday()), axis=1)
+
+dfheti = (df.groupby('Hét kezdet', as_index=False).mean())
+dfheti['Heti új elhunytak átlaga'] = dfheti['Napi új elhunyt']
+dfheti['Dátum'] = dfheti['Hét kezdet']
+print (dfheti)
+
+plot = dfheti.plot(x='Dátum', y='Heti új elhunytak átlaga')	
+
+
+fig = plot.get_figure()
+fig.savefig(BASEDIR + "/képek/Halálozasok.png", bbox_inches = "tight")
