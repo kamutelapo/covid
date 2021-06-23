3191e5b195f7167756209c4c79a943ed02da8261
Kamu Telapo <kamutelapo@freemail.hu>
2021-06-23 11:43:01 +0200

    Képek készítése

---
+++ képek_készítése.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+BASEDIR=`dirname $`
+
+$BASEDIR/kép_alapbetegség_nélküli_elhunytak.py
+$BASEDIR/kép_covid_halottak.py
+$BASEDIR/kép_covid_oltottak.py
+$BASEDIR/kép_elhunytak_átlag_életkora.py
+$BASEDIR/kép_elhunytak_életkor_szerinti_eloszlása.py
+
