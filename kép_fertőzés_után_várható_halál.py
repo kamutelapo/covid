2ca465b2f7f7c16c152aca75fb0d99f32796ce3e
Karai Csaba <cskarai@freemail.hu>
2021-06-27 20:13:19 +0200

    Fertőzés után várható halál

---
+++ kép_fertőzés_után_várható_halál.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+import numpy as np
+import datetime as dt
+import os
+
+BASEDIR=os.path.dirname(__file__)
+
+df = pd.read_csv(BASEDIR +"/adatok/covidadatok.csv", parse_dates=['Dátum'])
+
+korrelacio = []
+
+for napok in range(0,30):
+    if napok == 0:
+      c1 = df
+      c2 = df
+    else:
+      c1 = df.iloc[napok:, :]['Napi új elhunyt'].reset_index()
+      c2 = df.iloc[:-napok]['Napi új fertőzött'].reset_index()
+
+    dfr = c1[['Napi új elhunyt']]
+    dfr['Napi új fertőzött'] = c2['Napi új fertőzött']
+    
+    korrelacio.append([napok, dfr['Napi új fertőzött'].corr(dfr['Napi új elhunyt'])])
+    
+
+dfkorr = pd.DataFrame(korrelacio, columns=['Napok eltolása', 'Korreláció'])
+
+dfsorted=dfkorr.sort_values('Korreláció', ascending=False)
+
+varhato_halal = int(dfsorted['Napok eltolása'].head(1).mean())
+
+plot = dfkorr.plot(x='Napok eltolása', y='Korreláció', title="COVID fertőzés után várható halál (" + str(varhato_halal) + " nap)")	
+
+fig = plot.get_figure()
+fig.savefig(BASEDIR + "/képek/FertőzésUtánVárhatóHalál.png", bbox_inches = "tight")
