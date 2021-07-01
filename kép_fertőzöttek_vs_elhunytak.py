841d2e10281c04a336f91b5493deb62b680a77c6
ckarai <cskarai@freemail.hu>
2021-07-01 07:55:11 +0200

    Parazita tengelyek

--- kép_fertőzöttek_vs_elhunytak.py
+++ kép_fertőzöttek_vs_elhunytak.py
@@ -5,6 +5,9 @@ import pandas as pd
 import numpy as np
 import datetime as dt
 import os
+from mpl_toolkits.axes_grid1 import host_subplot
+import matplotlib.pyplot as plt
+
 
 BASEDIR=os.path.dirname(__file__)
 
@@ -12,11 +15,33 @@ df = pd.read_csv(BASEDIR +"/adatok/covidadatok.csv", parse_dates=['Dátum'])
 df['Hét kezdet'] = df.apply(lambda row: row['Dátum'] - dt.timedelta(days=row['Dátum'].weekday()), axis=1)
 
 dfheti = (df.groupby('Hét kezdet', as_index=False).mean())
-dfheti['Heti új fertőzöttek átlaga / 20'] = dfheti['Napi új fertőzött'] / 20
+dfheti['Heti új fertőzöttek átlaga'] = dfheti['Napi új fertőzött']
 dfheti['Heti új elhunytak átlaga'] = dfheti['Napi új elhunyt']
 dfheti['Dátum'] = dfheti['Hét kezdet']
 
-plot = dfheti.plot(x='Dátum', y=['Heti új fertőzöttek átlaga / 20','Heti új elhunytak átlaga'], title="COVID fertőzöttek és elhunytak")	
+pd.plotting.register_matplotlib_converters()
 
-fig = plot.get_figure()
+host = host_subplot(111)
+
+par = host.twinx()
+
+host.set_xlabel("Dátum")
+host.set_ylabel("Heti új fertőzöttek átlaga")
+host.set_title("COVID fertőzöttek és elhunytak")
+par.set_ylabel("Heti új elhunytak átlaga")
+par.set_ylim([0, 450])
+
+p1, = host.plot(dfheti['Dátum'], dfheti['Heti új fertőzöttek átlaga'], label="Heti új fertőzöttek átlaga")
+p2, = par.plot(dfheti['Dátum'], dfheti['Heti új elhunytak átlaga'], label="Heti új elhunytak átlaga")
+
+leg = plt.legend()
+
+host.yaxis.get_label().set_color(p1.get_color())
+leg.texts[0].set_color(p1.get_color())
+
+par.yaxis.get_label().set_color(p2.get_color())
+leg.texts[1].set_color(p2.get_color())
+
+fig = host.get_figure()
+fig.autofmt_xdate()
 fig.savefig(BASEDIR + "/képek/FertőzöttekVsElhunytak.png", bbox_inches = "tight")
--- kép_halálozási_ráta.py
+++ kép_halálozási_ráta.py
@@ -5,6 +5,8 @@ import pandas as pd
 import numpy as np
 import datetime as dt
 import os
+from mpl_toolkits.axes_grid1 import host_subplot
+import matplotlib.pyplot as plt
 
 BASEDIR=os.path.dirname(__file__)
 
@@ -21,12 +23,32 @@ fertozott['Hét kezdet'] = df.apply(lambda row: row['Dátum'] - dt.timedelta(day
 dfheti = (fertozott.groupby('Hét kezdet', as_index=False).mean())
 dfheti['Fertőzésben meghalt'] = dfheti.apply(lambda row: row['Fertőzésben meghalt'] if int(row['Fertőzésben meghalt']) >= 15 else np.NaN, axis=1)
 dfheti['Halálozási ráta'] = dfheti['Fertőzésben meghalt'] / dfheti['Napi új fertőzött']
-dfheti = dfheti.rename(columns = {'Hét kezdet': 'Dátum'}, inplace = False)
-dfheti['Heti új fertőzött átlaga / 500000'] = dfheti['Napi új fertőzött'] / 500000
-dfheti['Heti új elhunyt átlaga / 10000'] = dfheti['Napi új elhunyt'] / 10000
+dfheti = dfheti.rename(columns = {'Hét kezdet': 'Dátum', 'Napi új fertőzött': 'Heti új fertőzött átlaga'}, inplace = False)
 
-plot = dfheti.plot(x='Dátum', y=['Halálozási ráta', 'Heti új fertőzött átlaga / 500000'], title="COVID fertőzöttek halálozási rátája (halálozás 12 nappal eltolva)")	
+pd.plotting.register_matplotlib_converters()
 
-fig = plot.get_figure()
+host = host_subplot(111)
+
+par = host.twinx()
+
+host.set_xlabel("Dátum")
+host.set_ylabel("Halálozási ráta")
+host.set_title("COVID fertőzöttek halálozási rátája (halálozás 12 nappal eltolva)")
+par.set_ylabel("Heti új fertőzött átlaga")
+par.set_ylim([0, 80000])
+
+p1, = host.plot(dfheti['Dátum'], dfheti['Halálozási ráta'], label="Halálozási ráta")
+p2, = par.plot(dfheti['Dátum'], dfheti['Heti új fertőzött átlaga'], label="Heti új fertőzött átlaga")
+
+leg = plt.legend()
+
+host.yaxis.get_label().set_color(p1.get_color())
+leg.texts[0].set_color(p1.get_color())
+
+par.yaxis.get_label().set_color(p2.get_color())
+leg.texts[1].set_color(p2.get_color())
+
+fig = host.get_figure()
+fig.autofmt_xdate()
 fig.savefig(BASEDIR + "/képek/FertőzöttekHalálozásiRátája.png", bbox_inches = "tight")
 
