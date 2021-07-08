5a5c1481c771bf4964a52a11e39d8d46f0aa27aa
Karai Csaba <cskarai@freemail.hu>
2021-07-08 21:53:04 +0200

    További 7 napos mozgóátlag

--- kép_elhunytak_átlag_életkora.py
+++ kép_elhunytak_átlag_életkora.py
@@ -9,21 +9,26 @@ import os
 BASEDIR=os.path.dirname(__file__)
 
 df = pd.read_csv(BASEDIR +"/adatok/elhunytak_datummal.csv", parse_dates=['Dátum'])
+dfgrp = df.groupby('Dátum').agg(['sum', 'count'])
 
-df['Hét kezdet'] = df.apply(lambda row: row['Dátum'] - dt.timedelta(days=row['Dátum'].weekday()), axis=1)
+dfgrp['Elhunyt'] = dfgrp[('Kor', 'count')]
+dfgrp['Életkor összeg'] = dfgrp[('Kor', 'sum')]
+del dfgrp['Sorszám']
+del dfgrp['Nem']
+del dfgrp['Alapbetegségek']
+del dfgrp['Kor']
 
-dfgrp = df.groupby('Hét kezdet')
-dfhetihalott = dfgrp.size().to_frame('Heti elhunytak').reset_index()
+dfok = pd.DataFrame({'Dátum': pd.date_range(start=df['Dátum'].min(), end=df['Dátum'].max())})
 
-dfkor = dfgrp.mean().reset_index()
+dfgrp = pd.merge(dfgrp, dfok, left_on = 'Dátum', right_on = 'Dátum', how="outer").sort_values('Dátum').reset_index().fillna(0)
+del dfgrp['index']
 
-dfkor = pd.merge(dfkor, dfhetihalott, left_on = 'Hét kezdet', right_on = 'Hét kezdet')
+dfossz = dfgrp.rolling(7, center=True).sum()
+dfgrp['Heti elhunyt'] = dfossz['Elhunyt'].fillna(0)
+dfgrp['Átlag életkor'] = dfossz['Életkor összeg'] / dfossz['Elhunyt']
+dfgrp['Átlag életkor'] = dfgrp.apply(lambda row: float(row['Átlag életkor']) if int(row['Heti elhunyt']) >= 13 else np.NaN, axis=1)
 
-dfkor['Átlag életkor'] = dfkor.apply(lambda row: row['Kor'] if int(row['Heti elhunytak']) >= 10 else np.NaN, axis=1)
-
-dfkor = dfkor.rename(columns = {'Hét kezdet': 'Dátum'}, inplace = False)
-
-plot = dfkor.plot(x='Dátum', y='Átlag életkor', title='A COVID-ban elhunytak átlag életkora')
+plot = dfgrp.plot(x='Dátum', y='Átlag életkor', title='A COVID-ban elhunytak átlag életkora')
 
 fig = plot.get_figure()
 fig.savefig(BASEDIR + "/képek/ElhunytakÁtlagÉletkora.png", bbox_inches = "tight")
