e633d6bc45ba622b48d16758aec46a4012eb13c9
Karai Csaba <cskarai@freemail.hu>
2021-09-19 19:40:22 +0200

    Dátumfüggetlenné tétel

--- kép_ksh_halálozás_vs_oltás.py
+++ kép_ksh_halálozás_vs_oltás.py
@@ -7,16 +7,18 @@ import datetime as dt
 import os
 from mpl_toolkits.axes_grid1 import host_subplot
 import matplotlib.pyplot as plt
+from datetime import timedelta
 
 BASEDIR=os.path.dirname(__file__)
 
 df = pd.read_csv(BASEDIR +"/adatok/stadat-halalozas-elokeszitve.csv", parse_dates=['A hét kező napja', 'A hét záró napja'], delimiter=';')
-dfweeks = df[(df['A hét sorszáma'] < 29.0) | (df['A hét sorszáma'] > 35.0)]
+maxdate = df['A hét kező napja'].max() + timedelta(days = -35)
+df = df[df['A hét kező napja'] <= maxdate]
 
-df2020 = dfweeks[dfweeks['A hét záró napja'] > "2020-08-30"]
+df2020 = df[df['A hét záró napja'] > "2020-08-30"]
 covid_elhunytak = df2020[['A hét kező napja', 'A hét sorszáma', 'Összesen összesen']].rename(columns = {'Összesen összesen': 'Elhunytak'}, inplace = False)
 
-dfatlag = dfweeks[dfweeks['A hét záró napja'] < "2020-01-01"]
+dfatlag = df[df['A hét záró napja'] < "2020-01-01"]
 
 dfatlag = dfatlag.groupby('A hét sorszáma').mean().reset_index()
 atlag_elhunytak = dfatlag[['A hét sorszáma', 'Összesen összesen']].rename(columns = {'Összesen összesen': 'KSH 5 éves átlag'}, inplace = False)
@@ -25,15 +27,14 @@ dfkozos = pd.merge(covid_elhunytak, atlag_elhunytak, left_on = 'A hét sorszáma
 dfkozos = dfkozos.rename(columns = {'A hét kező napja': 'Dátum'}, inplace = False)
 dfkozos['KSH többlet']=dfkozos['Elhunytak'] - dfkozos['KSH 5 éves átlag']
 
-df = pd.read_csv(BASEDIR +"/adatok/covidadatok.csv", parse_dates=['Dátum'])
+df = pd.read_csv(BASEDIR +"/adatok/hiradatok.csv", parse_dates=['Dátum'])
 df['Dátum'] = df.apply(lambda row: row['Dátum'] - dt.timedelta(days=row['Dátum'].weekday()), axis=1)
 dfheti = (df.groupby('Dátum', as_index=False).sum())
 
 dfheti = dfheti.rename(columns = {'Napi új beoltott': 'Heti új beoltott'}, inplace = False)
-dfheti = dfheti[(dfheti['Dátum'] < "2021-07-25") & (dfheti['Dátum'] > "2020-08-30")]
+dfheti = dfheti[(dfheti['Dátum'] <= maxdate) & (dfheti['Dátum'] > "2020-08-30")]
 
 dfkozos = pd.merge(dfkozos, dfheti, left_on = 'Dátum', right_on = 'Dátum')
-
 
 pd.plotting.register_matplotlib_converters()
 
