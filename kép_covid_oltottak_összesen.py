cec4d8fc892be2e27451d8e399b41c04b0ee3a07
Kamu Telapo <kamutelapo@freemail.hu>
2021-11-15 10:15:28 +0100

    6 hónapon belül oltottak

--- kép_covid_oltottak_összesen.py
+++ kép_covid_oltottak_összesen.py
@@ -11,8 +11,12 @@ BASEDIR=os.path.dirname(__file__)
 df = pd.read_csv(BASEDIR +"/adatok/hiradatok.csv", parse_dates=['Dátum'])
 df = df[df['Dátum'] >= "2021-01-01"].reset_index()
 
-plot = df.plot(x='Dátum', y=['Beoltottak', 'Kétszer oltottak', 'Háromszor oltottak'], ylim=[0, 6000000],
-               title='A COVID ellen beoltottak összesen', color=['blue', 'orange', 'green'], grid=True)
+df['6 hónapon belül oltottak'] = ((df['Napi új másodszor oltott'] + df['Napi új harmadszor oltott']).rolling(min_periods=1, window=180).sum().fillna(0))
+
+
+plot = df.plot(x='Dátum', y=['6 hónapon belül oltottak', 'Beoltottak', 'Kétszer oltottak', 'Háromszor oltottak'],
+               ylim=[0, 6000000], title='A COVID ellen beoltottak összesen', color=['magenta', 'blue', 'orange', 'green'],
+               grid=True)
 
 fig = plot.get_figure()
 fig.savefig(BASEDIR + "/képek/BeoltottakÖsszesen.png", bbox_inches = "tight")
