f528826739a484c83e00f7d97a8dcad29f342763
ckarai <cskarai@freemail.hu>
2021-11-23 07:49:46 +0100

    Adatok javítása

--- kép_ksh_halálozás_vs_oltás.py
+++ kép_ksh_halálozás_vs_oltás.py
@@ -23,13 +23,14 @@ dfatlag = df[df['A hét záró napja'] < "2020-01-01"]
 dfatlag = dfatlag.groupby('A hét sorszáma').mean().reset_index()
 atlag_elhunytak = dfatlag[['A hét sorszáma', 'Összesen összesen']].rename(columns = {'Összesen összesen': 'KSH 5 éves átlag'}, inplace = False)
 
-dfkozos = pd.merge(covid_elhunytak, atlag_elhunytak, left_on = 'A hét sorszáma', right_on = 'A hét sorszáma')
+dfkozos = pd.merge(covid_elhunytak, atlag_elhunytak, left_on = 'A hét sorszáma', right_on = 'A hét sorszáma', how = 'left')
 dfkozos = dfkozos.rename(columns = {'A hét kező napja': 'Dátum'}, inplace = False)
 dfkozos['KSH többlet']=dfkozos['Elhunytak'] - dfkozos['KSH 5 éves átlag']
 
 df = pd.read_csv(BASEDIR +"/adatok/hiradatok.csv", parse_dates=['Dátum'])
 df['Dátum'] = df.apply(lambda row: row['Dátum'] - dt.timedelta(days=row['Dátum'].weekday()), axis=1)
 dfheti = (df.groupby('Dátum', as_index=False).sum())
+dfheti['Napi új beoltott'] = dfheti['Napi új beoltott'] + dfheti['Napi új másodszor oltott'] + dfheti['Napi új harmadszor oltott']
 
 dfheti = dfheti.rename(columns = {'Napi új beoltott': 'Heti új beoltott'}, inplace = False)
 dfheti = dfheti[(dfheti['Dátum'] <= maxdate) & (dfheti['Dátum'] > "2020-08-30")]
@@ -47,7 +48,7 @@ host.set_title("COVID halálozások vs oltások")
 host.set_ylabel("KSH többlet")
 host.set_ylim([-250, 5500])
 par.set_ylabel("Heti új beoltott")
-par.set_ylim([-25000, 550000])
+par.set_ylim([-40000, 880000])
 
 p1, = host.plot(dfkozos['Dátum'], dfkozos['KSH többlet'], label="KSH többlet", color='blue')
 p2, = par.plot(dfkozos['Dátum'], dfkozos['Heti új beoltott'], label="Heti új beoltott", color='green')
