6cbf099eb328df8051860ceb72382e37680bd1dd
ckarai <cskarai@freemail.hu>
2021-11-24 07:48:30 +0100

    Kicsit több kód

--- anglia/adatkinyerés.py
+++ anglia/adatkinyerés.py
@@ -3,11 +3,32 @@
 
 import sys
 import os
+import re
+
+def processReport(hetszama, content):
+    print ("TODO", hetszama)
+    pass
+
 
 FILEDIR=sys.argv[1]
+
+SZAM_PATTERN = re.compile(r'^.*week\s*_?\-?\s*(\d+).*$')
 
 for filename in os.listdir(FILEDIR):
   if filename.endswith(".txt"): 
     path = os.path.join(FILEDIR, filename)
-    print (path)
+    
+    text_file = open(path, "r")
+    lines = text_file.readlines()
+    text_file.close()
+    
+    mtch = SZAM_PATTERN.match(filename)    
+    if mtch:
+      szam = int(mtch.group(1))
+      processReport(szam, lines)
+    else:
+      print ("Programhiba", filename)
+      quit()
+    
+print ("TODO: utofeldolgozas")
 
