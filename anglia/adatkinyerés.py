61d5cb20ce51089bcaa51a3a6c4e5e2ad7d15bf5
ckarai <cskarai@freemail.hu>
2021-11-24 07:36:45 +0100

    Iterálás a fájlokon

--- anglia/adatkinyerés.py
+++ anglia/adatkinyerés.py
@@ -2,7 +2,12 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import os
 
 FILEDIR=sys.argv[1]
 
-print ("TODO:", FILEDIR)
+for filename in os.listdir(FILEDIR):
+  if filename.endswith(".txt"): 
+    path = os.path.join(FILEDIR, filename)
+    print (path)
+
